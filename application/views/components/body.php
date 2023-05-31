bb16277927254d21ef7bb80d2d4e6ec2b8546f70
Nohan <nohanrazafinjato@gmail.com>
2023-05-31 20:39:33 +0300

    saika nav

--- application/views/components/body.php
+++ application/views/components/body.php
@@ -5,10 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="<?php echo base_url('assets/img/icon.png'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
     <title><?php echo $title; ?></title>
+
 </head>
+
 <body>
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-3 aside d-flex justify-content-center">
+                <div class="side d-flex flex-column align-items-center p-2">
+                    <img src="<?php echo base_url('assets/img/nav-icon.png'); ?>" alt="vegmarket-icon" class="w-75 p-3 mb-1">
     
+                    <ul class="w-75 menu">
+                        <li>
+                            <a href="#">Tableau de bord</a>
+                           
+                        </li>
+                    </ul> 
+                </div>
+            </div>
+
+            <div class="col-9">
+                <main class="p-3">
+                    <!-- Content -->
+                    dfdf
+                </main>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
