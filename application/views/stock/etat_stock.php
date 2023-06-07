1c95ab48661627a9fa4fd7bd9a5b5eaa63c17e12
Nohan <nohanrazafinjato@gmail.com>
2023-06-07 14:37:06 +0300

    stock

--- application/views/stock/etat_stock.php
+++ application/views/stock/etat_stock.php
@@ -5,12 +5,12 @@
             <?php
             foreach ($etat as $e) { ?>
                 <div class="py-2 etat-detail">
-                    <h4><?php echo $e['adresse']; ?></h4>
+                    <h4><i class="fa-solid fa-warehouse" style="font-size:14px; color:#264026;"></i> <?php echo $e['adresse']; ?></h4>
                     <div class="d-flex flex-wrap gap-2">
                         <?php
                         foreach ($e['element'] as $element) { ?>
                             <div class="etat-card">
-                                <h5><?php echo $element['produit']; ?></h5>
+                                <h5> <?php echo $element['produit']; ?></h5>
                                 <b>Quantité stocké : <?php echo $element['instock']; ?> <span style="font-size:10px; background:transparent;">x1000 Kg</span></b>
                                 <br>
                                 <b>Quantité restante : <?php echo $element['qtt_max'] - $element['instock']; ?> <span style="font-size:10px; background:transparent;">x1000 Kg</span></b>
