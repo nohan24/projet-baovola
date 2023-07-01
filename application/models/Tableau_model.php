b170838088b91dd32a541c625391395b9612c0ad
RASOLOARISON Tahiry Johary <118507545+Achyl2051@users.noreply.github.com>
2023-07-01 12:45:58 +0300

    maty_jiro_dash

--- application/models/Tableau_model.php
+++ application/models/Tableau_model.php
@@ -1,4 +1,15 @@
 <?php
 class Tableau_model extends CI_Model
 {
+    public function getStat()
+    {
+        $result=[];
+        for($i=0;$i<12;$i++)
+        {
+            $sql="SELECT produitid, SUM(quantite) FROM sortie_stock WHERE EXTRACT(MONTH FROM date_sortie) = "+$i+" GROUP BY produitid, type_sortie";
+            $query=$this->db->query($sql);
+            $result[$i]=$query->result_array();
+        }
+        return $result;
+    }
 }
